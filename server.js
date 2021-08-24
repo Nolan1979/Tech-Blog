7ff3cfa648274430db53b61ecb1ab5e0bc6028ce
Josh Nolan <nolanjosh1979@gmail.com>
2021-08-24 11:55:53 -0500

    more scaffolding added plus db created/main handlebars

--- server.js
+++ server.js
@@ -5,15 +5,20 @@ const hbs = exphbs.create({});
 const path = require('path');
 
 
+const sequelize = require('./config/connection');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/dish-routes'));
 
-app.listen(PORT, () => {
-    console.log('Server listening on: http://localhost:' + PORT);
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
