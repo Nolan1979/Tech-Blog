c9718889df9aacb0ca210c7aa66bf87ca89e5eb0
Josh Nolan <nolanjosh1979@gmail.com>
2021-08-24 13:14:25 -0500

    created dotenv

--- server.js
+++ server.js
@@ -6,9 +6,20 @@ const path = require('path');
 
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -20,5 +31,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening', + PORT));
 });
